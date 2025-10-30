6b062e0eda42dddc28504fbb4e982f13593f7f3a
kad-0312 <khoaanhdo0312@gmail.com>
2025-10-30 11:08:37 +0700

    delete: Delete member section

--- app/(frontend)/(router)/about-us/page.tsx
+++ app/(frontend)/(router)/about-us/page.tsx
@@ -4,7 +4,6 @@ import ManagementBoard from "./components/execute-board/managementBoard";
 import III from "./components/III/III";
 import HistorySection from "./components/history";
 import FinTechInTheEyes from "./components/inTheEyes";
-import Members from "./components/members";
 import { ClubMentors } from "./components/mentors";
 import VisionMission from "./components/vision-mission/missionVision";
 import HallOfFamePage from "./components/hallOfFamePage";
@@ -20,7 +19,6 @@ const AboutUs = () => {
 			<ExecuteBoard />
 			<ManagementBoard />
 			<HallOfFamePage />
-			{/* <Members /> */}
 		</>)
 };
 
